c4786e5ff842887f6357b0c002fcdf822f681a3f
cabraham1 <christopherabraham8@gmail.com>
2023-03-16 11:41:35 +0100

    update

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const body = document.querySelector('.body')
 
-display = () => {
-    const div = document.createElement('div')
-    div.innerHTML = `
+const info = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  return display(data);
+};
+
+
+const display = (data) => {
+  data.map((post) => {
+const div = document.createElement("div");
+div.innerHTML = `
     <div class='main-title'>
             <h1>My API Table</h1>
         </div>
-    
        <div  class= 'bread'>
-        <h2>users:1</h2>
-        <h2>Title:bread </h2>
-        <h2>body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis placeat provident ipsam?</h2>
+        <h2>users:${post.id}</h2>
+        <h2>Title:${post.title} </h2>
+        <h2>body: ${post.body}</h2>
        </div>
-    
-       
-       <div class= 'wine'>
-        <h2>users:2</h2>
-        <h2>Title: wine</h2>
-        <h2>body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias iure doloribus impedit nobis vel expedita.</h2>
-       </div> 
-    
-       <div class= 'malt'>
-        <h2>users:3</h2>
-        <h2>Title: malt</h2>
-        <h2>body: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius ab molestias eveniet ut! Voluptatem aspernatur quasi hic!</h2>
-       </div>
-    `
 
+    `;
     body.appendChild(div)
+  })
 }
 
-display();
+info()
+
